4f2171414364c017c648814c5316e05d1902e236
hudsonhs <hharriman.smith@gmail.com>
2024-11-23 10:11:30 -0500

    Single-lines can be done in a single line

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,9 @@ import List from "./List"
 import { mockTodoList } from "./mockData"
 
 export default function Home() {
-  const notDone = mockTodoList.items.filter(item =>
-      !item.completed
-  )
-  const completedTodos = mockTodoList.items.filter(item =>
-    item.completed
-  )
+  const notDone = mockTodoList.items.filter(item => !item.completed)
+  const completedTodos = mockTodoList.items.filter(item => item.completed)
+  
   return (
     <>
     <h1>{mockTodoList.title}</h1>
